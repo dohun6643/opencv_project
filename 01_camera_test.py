af21d1d0be2f2fb3e68853a21c30313a04eab36d
lapass50025 <lapass50025@gmail.com>
2020-12-17 00:29:58 +0900

    added camera_test

--- 01_camera_test.py
+++ 01_camera_test.py
@@ -1,62 +1,3 @@
-## 1. 참고 자료
---------------------------------------------------
-- CSI-Camera
-https://github.com/JetsonHacksNano/CSI-Camera
-
-
-## 2. 프로그램 설치하기
---------------------------------------------------
-- $ sudo apt install ffmpeg
-- $ sudo apt install v4l-utils
-- $ sudo apt install python3-opencv -y
-- $ sudo apt install tightvncserver
-- $ sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
-
-
-## 3. 비디오 장치 정보 출력하기
---------------------------------------------------
-- $ sudo v4l2-ctl --list-devices
-> vi-output, imx219 7-0010 (platform:54080000.vi:0):
->        /dev/video0
-
-- $ v4l2-ctl --device=/dev/video0 --all
-> Driver Info (not using libv4l2):
->>        Driver name   : tegra-video
->>        Card type     : vi-output, imx219 7-0010
->>        Bus info      : platform:54080000.vi:0
->>        Driver version: 4.9.140
-
->>        Capabilities  : 0x84200001
->>>                Video Capture
->>>                Streaming
->>>                Extended Pix Format
->>>                Device Capabilities
-
->>        Device Caps   : 0x04200001
->>>                Video Capture
->>>                Streaming
->>>                Extended Pix Format
-> Priority: 2
-
-> Video input : 0 (Camera 0: no power)
-
-> Format Video Capture:
->>        Width/Height      : 3264/2464
->>        Pixel Format      : 'RG10'
->>        Field             : None
->>        Bytes per Line    : 6528
->>        Size Image        : 16084992
->>        Colorspace        : sRGB
->>        Transfer Function : Default (maps to sRGB)
->>        YCbCr/HSV Encoding: Default (maps to ITU-R 601)
->>        Quantization      : Default (maps to Full Range)
-
-## 4. 카메라 테스트하기
---------------------------------------------------
-- 카메라와 연결하기 위해 GStreamer 파이프라인을 구한다.
-
-<pre>
-(예)
 # 모듈 불러오기
 import cv2 as cv
 import numpy as np
@@ -143,4 +84,5 @@ def main():
 # 메인 함수 호출하기
 if __name__ == "__main__":
     main()
-</pre>
+
+
