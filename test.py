5b0dd623f7bd2db1eb38e2e92059caeea24e0ad3
Kunhee Lee <kunheeuk@naver.com>
2020-12-17 17:04:56 +0900

    try

--- test.py
+++ test.py
@@ -80,3 +80,150 @@ Runcar(4, 0.1, 10, 0.2, 0.2)
 Runcar(4, 0.1, 10, 0.2, 0.2)
 time.sleep(3)
 robot.stop()
+
+
+# jupyter copy
+from jetbot import Robot
+from jetbot import Camera
+from IPython.display import display
+from jetbot import bgr8_to_jpeg
+import time, numpy as np, cv2 as cv
+import ipywidgets.widgets as widgets
+import matplotlib.pyplot as plt
+
+robot = Robot()
+
+camera = Camera.instance(width=300, height=300)
+originalCam = widgets.Image(format='jpeg', width=300, height=300)
+grayCam = widgets.Image(format='jpeg', width=300, height=300)
+threshCam = widgets.Image(format='jpeg', width=300, height=300)
+cannyCam = widgets.Image(format='jpeg', width=300, height=300)
+
+def convertGray(img):
+    imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    imgBlur = cv.GaussianBlur(imgGray, (5,5), 0)
+    return imgBlur
+
+def applyThreshold(convertGray):
+    ret,thresh = cv.threshold(convertGray, 100, 255, cv.THRESH_OTSU + cv.THRESH_BINARY_INV)
+    return thresh
+
+def applyCanny(applyThreshold):
+    canny = cv.Canny(applyThreshold, 0, 1500)
+    kernel = np.ones((5,5), np.uint8)
+    imgDilate = cv.dilate(canny, kernel, iterations=1)
+    return imgDilate
+
+def detecting(img, applyCanny):
+    contours, _ = cv.findContours(applyCanny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+    for contour in contours:
+        x, y, w, h = cv.boundingRect(contour)
+        epsilon = 0.01 * cv.arcLength(contour, True)
+        approx = cv.approxPolyDP(contour, epsilon, True)
+        cv.drawContours(img, [approx], 0, (255, 0, 0), 2)
+    return img
+
+display(widgets.HBox((originalCam, grayCam)))
+display(widgets.HBox((threshCam, cannyCam)))
+
+while True :
+    img = (camera.value)
+    grayimg = convertGray(img)
+    threshimg = applyThreshold(grayimg)
+    cannyimg = applyCanny(threshimg)
+    detectimg = detecting(img, cannyimg)
+    
+    originalCam.value= bgr8_to_jpeg(img,100)
+    grayCam.value = bgr8_to_jpeg(grayimg,100)
+    threshCam.value = bgr8_to_jpeg(threshimg,100)
+    cannyCam.value = bgr8_to_jpeg(cannyimg,100)
+    
+    time.sleep(0.1)
+
+# ---------------------------------------------------------------------------
+from jetbot import Robot
+from jetbot import Camera
+from IPython.display import display
+from jetbot import bgr8_to_jpeg
+import time, traitlets, subprocess, os
+import ipywidgets.widgets as widgets
+import numpy as np, cv2 as cv
+robot = Robot()
+
+def stop(change):
+    robot.stop()
+    
+def forward(change):
+    robot.set_motors(0.11,0.1) # 좌우 모터 출력값이 달라 직선으로 가게하기 위해 개별적으로 모터 제어
+                               # 모터의 최소 출력값이 0.1
+
+def turnLeft(change): # 약 90도 좌향좌
+    robot.right_motor.value = 0.1
+    time.sleep(2.4) # 이 기체는 왼쪽 모터의 출력이 조금 더 쎈것 같음 
+    robot.stop()
+
+def turnRight(change): # 약 90도 우향우
+    robot.left_motor.value = 0.1
+    time.sleep(2.8)
+    robot.stop()
+
+def detourLeft(change): # 좌로 우회
+    for i in range(0,5):
+        robot.set_motors(0.15, 0.1)
+        time.sleep(1)
+    
+    robot.stop()
+    
+def detourRight(change): # 우로 우회
+    for i in range(0,5):
+        robot.set_motors(0.1, 0.15)
+        time.sleep(1)
+    
+    robot.stop()
+    
+def snapshot(change):
+    # save snapshot onclick
+#     if change['new']:
+    strpath = 'snapshots/'
+    dirobj = os.listdir(strpath)
+    count = len(dirobj)
+    file_path = 'snapshots/%05d.jpg' %count
+        
+        # write snapshot to file (we use image value instead of camera because it's already in JPEG format)
+    with open(file_path, 'wb') as f:
+        filechanged = bgr8_to_jpeg(camera.value)
+        f.write(filechanged)
+            
+        # display snapshot that was saved
+    snapshot_image.value = filechanged
+
+    # create buttons
+button_layout = widgets.Layout(width='100px', height='80px', align_self='center')
+stop_button = widgets.Button(description='stop', button_style='danger', layout=button_layout)
+forward_button = widgets.Button(description='forward', layout=button_layout)
+snapshot_button = widgets.Button(description='snapshot', layout=button_layout)
+
+detourLeft_button = widgets.Button(description='detourLeft', layout=button_layout)
+detourRight_button = widgets.Button(description='detourRight', layout=button_layout)
+
+turnLeft_button = widgets.Button(description='turnLeft', layout=button_layout)
+turnRight_button = widgets.Button(description='turnRight', layout=button_layout)
+
+# display buttons
+middle_box = widgets.HBox([detourLeft_button, stop_button, detourRight_button], layout=widgets.Layout(align_self='center'))
+bottom_box = widgets.HBox([turnLeft_button, snapshot_button, turnRight_button], layout=widgets.Layout(align_self='center'))
+controls_box = widgets.VBox([forward_button, middle_box, bottom_box])
+
+display(controls_box)
+
+# link buttons to actions
+stop_button.on_click(stop)
+forward_button.on_click(forward)
+
+detourLeft_button.on_click(detourLeft)
+detourRight_button.on_click(detourRight)
+
+turnLeft_button.on_click(turnLeft)
+turnRight_button.on_click(turnRight)
+
+snapshot_button.on_click(snapshot)
